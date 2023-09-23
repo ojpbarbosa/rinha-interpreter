277e6e7568671285b367638ee4690e89a91ce82f
ojpbarbosa <joaopedrofbarbosa@protonmail.com>
2023-09-23 18:49:44 -0300

    style: use rust-analyzer

--- src/ast.rs
+++ src/ast.rs
@@ -2,32 +2,32 @@ use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
 pub struct File {
-  pub name: String,
-  pub expression: Term,
-  pub location: Location,
+    pub name: String,
+    pub expression: Term,
+    pub location: Location,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct Location {
-  pub start: i32,
-  pub end: i32,
-  pub filename: String,
+    pub start: i32,
+    pub end: i32,
+    pub filename: String,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct Str {
-  pub value: String,
-  pub location: Location,
+    pub value: String,
+    pub location: Location,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct Print {
-  pub value: Box<Term>,
+    pub value: Box<Term>,
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(tag = "kind")]
 pub enum Term {
-  Str(Str),
-  Print(Print),
+    Str(Str),
+    Print(Print),
 }
