41dfac8698479fb5579ff2c3397a4216ea184f9e
ojpbarbosa <joaopedrofbarbosa@protonmail.com>
2023-09-24 14:20:56 -0300

    feat(interpreter): implement binary evaluation

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -2,6 +2,7 @@ use std::fmt::Display;
 
 use crate::ast;
 
+#[derive(Debug)]
 pub enum Value {
     Int(i32),
     Bool(bool),
@@ -21,38 +22,6 @@ impl Display for Value {
     }
 }
 
-impl PartialEq for Value {
-    fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (Value::Int(a), Value::Int(b)) => a == b,
-            (Value::Bool(a), Value::Bool(b)) => a == b,
-            (Value::Str(a), Value::Str(b)) => a == b,
-            (Value::Tuple(a), Value::Tuple(b)) => a == b,
-            _ => false,
-        }
-    }
-}
-
-impl ast::Term {
-    pub fn location(&self) -> &ast::Location {
-        match self {
-            ast::Term::Int(t) => &t.location,
-            ast::Term::Str(t) => &t.location,
-            ast::Term::Bool(t) => &t.location,
-            ast::Term::Print(t) => &t.location,
-            ast::Term::Binary(t) => &t.location,
-            ast::Term::If(t) => &t.location,
-            ast::Term::Let(t) => &t.location,
-            ast::Term::Var(t) => &t.location,
-            ast::Term::Function(t) => &t.location,
-            ast::Term::Call(t) => &t.location,
-            ast::Term::Tuple(t) => &t.location,
-            ast::Term::First(t) => &t.location,
-            ast::Term::Second(t) => &t.location,
-        }
-    }
-}
-
 #[derive(Debug)]
 pub struct RuntimeError {
     pub message: String,
@@ -60,13 +29,179 @@ pub struct RuntimeError {
     pub kind: RuntimeErrorKind,
 }
 
+impl Display for RuntimeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}: {} at {}",
+            self.kind, self.message, self.location.start,
+        )
+    }
+}
+
 #[derive(Debug)]
 pub enum RuntimeErrorKind {
     ArgumentError,
     DivisionByZero,
-    UnknowIdentifier(ast::Var),
     InvalidBinaryOperation,
-    InvalidNumberOfArguments(ast::Function, ast::Location),
+}
+
+impl Display for RuntimeErrorKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            RuntimeErrorKind::ArgumentError => write!(f, "Argument Error"),
+            RuntimeErrorKind::DivisionByZero => write!(f, "Division by Zero"),
+            RuntimeErrorKind::InvalidBinaryOperation => write!(f, "Invalid Binary Operation"),
+        }
+    }
+}
+
+fn invalid_numeric_op(op: String, a: &Value, b: &Value, location: ast::Location) -> RuntimeError {
+    RuntimeError {
+        message: format!("Cannot {:?} {:?} and {:?}", op, a, b),
+        location,
+        kind: RuntimeErrorKind::InvalidBinaryOperation,
+    }
+}
+
+fn divison_by_zero(message: String, location: ast::Location) -> RuntimeError {
+    RuntimeError {
+        message,
+        location,
+        kind: RuntimeErrorKind::DivisionByZero,
+    }
+}
+
+fn invalid_comparison(a: &Value, b: &Value, location: ast::Location) -> RuntimeError {
+    RuntimeError {
+        message: format!("Cannot compare {:?} and {:?}", a, b),
+        location,
+        kind: RuntimeErrorKind::InvalidBinaryOperation,
+    }
+}
+
+impl Value {
+    pub fn add(&self, other: &Self) -> Result<Value, RuntimeError> {
+        match (&self, &other) {
+            (Value::Int(a), Value::Int(b)) => Ok(Value::Int(a + b)),
+            (a, b) => Ok(Value::Str(format!("{a}{b}"))),
+        }
+    }
+
+    pub fn sub(&self, other: &Self, location: ast::Location) -> Result<Value, RuntimeError> {
+        match (&self, &other) {
+            (Value::Int(a), Value::Int(b)) => Ok(Value::Int(a - b)),
+            (a, b) => Err(invalid_numeric_op(String::from("subtract"), a, b, location)),
+        }
+    }
+
+    pub fn mul(&self, other: &Self, location: ast::Location) -> Result<Value, RuntimeError> {
+        match (&self, &other) {
+            (Value::Int(a), Value::Int(b)) => Ok(Value::Int(a * b)),
+            (a, b) => Err(invalid_numeric_op(String::from("multiply"), a, b, location)),
+        }
+    }
+
+    pub fn div(&self, other: &Self, location: ast::Location) -> Result<Value, RuntimeError> {
+        match (&self, &other) {
+            (Value::Int(a), Value::Int(b)) => {
+                if b == &0 {
+                    Err(divison_by_zero(
+                        format!("Cannot divide {} by 0", a),
+                        location,
+                    ))
+                } else {
+                    Ok(Value::Int(a / b))
+                }
+            }
+            (a, b) => Err(invalid_numeric_op(String::from("divide"), a, b, location)),
+        }
+    }
+
+    pub fn rem(&self, other: &Self, location: ast::Location) -> Result<Value, RuntimeError> {
+        match (&self, &other) {
+            (Value::Int(a), Value::Int(b)) => {
+                if b == &0 {
+                    Err(divison_by_zero(
+                        format!("Cannot calculate remainder of {} dividing by 0", a),
+                        location,
+                    ))
+                } else {
+                    Ok(Value::Int(a % b))
+                }
+            }
+            (a, b) => Err(invalid_numeric_op(
+                String::from("calculate remainder of"),
+                a,
+                b,
+                location,
+            )),
+        }
+    }
+
+    pub fn eq(&self, other: &Self, location: ast::Location) -> Result<Value, RuntimeError> {
+        match (self, other) {
+            (Value::Int(a), Value::Int(b)) => Ok(Value::Bool(a == b)),
+            (Value::Str(a), Value::Str(b)) => Ok(Value::Bool(a == b)),
+            (Value::Bool(a), Value::Bool(b)) => Ok(Value::Bool(a == b)),
+            (a, b) => Err(invalid_comparison(a, b, location)),
+        }
+    }
+
+    pub fn neq(&self, other: &Self, location: ast::Location) -> Result<Value, RuntimeError> {
+        match (self, other) {
+            (Value::Int(a), Value::Int(b)) => Ok(Value::Bool(a != b)),
+            (Value::Str(a), Value::Str(b)) => Ok(Value::Bool(a != b)),
+            (Value::Bool(a), Value::Bool(b)) => Ok(Value::Bool(a != b)),
+            (a, b) => Err(invalid_comparison(a, b, location)),
+        }
+    }
+
+    pub fn lt(&self, other: &Self, location: ast::Location) -> Result<Value, RuntimeError> {
+        match (self, other) {
+            (Value::Int(a), Value::Int(b)) => Ok(Value::Bool(a < b)),
+            (Value::Str(a), Value::Str(b)) => Ok(Value::Bool(a < b)),
+            (a, b) => Err(invalid_comparison(a, b, location)),
+        }
+    }
+
+    pub fn gt(&self, other: &Self, location: ast::Location) -> Result<Value, RuntimeError> {
+        match (self, other) {
+            (Value::Int(a), Value::Int(b)) => Ok(Value::Bool(a > b)),
+            (Value::Str(a), Value::Str(b)) => Ok(Value::Bool(a > b)),
+            (a, b) => Err(invalid_comparison(a, b, location)),
+        }
+    }
+
+    pub fn lte(&self, other: &Self, location: ast::Location) -> Result<Value, RuntimeError> {
+        match (self, other) {
+            (Value::Int(a), Value::Int(b)) => Ok(Value::Bool(a <= b)),
+            (Value::Str(a), Value::Str(b)) => Ok(Value::Bool(a <= b)),
+            (a, b) => Err(invalid_comparison(a, b, location)),
+        }
+    }
+
+    pub fn gte(&self, other: &Self, location: ast::Location) -> Result<Value, RuntimeError> {
+        match (self, other) {
+            (Value::Int(a), Value::Int(b)) => Ok(Value::Bool(a >= b)),
+            (Value::Str(a), Value::Str(b)) => Ok(Value::Bool(a >= b)),
+            (a, b) => Err(invalid_comparison(a, b, location)),
+        }
+    }
+
+    pub fn and(&self, other: &Self, location: ast::Location) -> Result<Value, RuntimeError> {
+        match (self, other) {
+            (Value::Bool(a), Value::Bool(b)) => Ok(Value::Bool(*a && *b)),
+            (a, b) => Err(invalid_comparison(a, b, location)),
+        }
+    }
+
+    pub fn or(&self, other: &Self, location: ast::Location) -> Result<Value, RuntimeError> {
+        match (self, other) {
+            (Value::Bool(a), Value::Bool(b)) => Ok(Value::Bool(*a || *b)),
+            (a, b) => Err(invalid_comparison(a, b, location)),
+        }
+    }
 }
 
 fn evaluate(term: ast::Term) -> Result<Value, RuntimeError> {
@@ -113,7 +248,25 @@ fn evaluate_call(call: ast::Call) -> Result<Value, RuntimeError> {
 }
 
 fn evaluate_binary(binary: ast::Binary) -> Result<Value, RuntimeError> {
-    todo!();
+    let lhs = evaluate(binary.lhs)?;
+    let rhs = evaluate(binary.rhs)?;
+
+    let location = binary.location;
+    match binary.op {
+        ast::BinaryOp::Add => lhs.add(&rhs),
+        ast::BinaryOp::Sub => lhs.sub(&rhs, location),
+        ast::BinaryOp::Mul => lhs.mul(&rhs, location),
+        ast::BinaryOp::Div => lhs.div(&rhs, location),
+        ast::BinaryOp::Rem => lhs.rem(&rhs, location),
+        ast::BinaryOp::Eq => lhs.eq(&rhs, location),
+        ast::BinaryOp::Neq => lhs.neq(&rhs, location),
+        ast::BinaryOp::Lt => lhs.lt(&rhs, location),
+        ast::BinaryOp::Lte => lhs.lte(&rhs, location),
+        ast::BinaryOp::Gt => lhs.gt(&rhs, location),
+        ast::BinaryOp::Gte => lhs.gte(&rhs, location),
+        ast::BinaryOp::And => lhs.and(&rhs, location),
+        ast::BinaryOp::Or => lhs.or(&rhs, location),
+    }
 }
 
 fn evaluate_function(function: ast::Function) -> Result<Value, RuntimeError> {
@@ -134,5 +287,7 @@ fn evaluate_var(var: ast::Var) -> Result<Value, RuntimeError> {
 
 pub fn interpret_program(program: ast::File) {
     // todo: create context
-    let _ = evaluate(program.expression);
+    if let Err(error) = evaluate(program.expression) {
+        println!("{}", error);
+    }
 }
